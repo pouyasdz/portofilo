c739669f04f4017319661fadc0e3254110056a61
pouyasdz <qpdevil83@gmail.com>
2023-09-05 17:02:37 +0330

    complete home page

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -4,7 +4,7 @@ export const header = `
 <nav class="navbar">
 <ul>
     <li><a href="${isIndex?'./index.html':'../index.html'}"><i class="fi fi-rr-home"></i> <span>Home</span></a></li>
-    <li><a href="${isIndex?'./pages/contact_to_me.html':'./contact_to_me.html'}"><i class="fi fi-rr-phone-call"></i> <span>Contact To Me</span></a></li>
+    <li><a href="${isIndex?'./pages/contact_to_me.html':'./contact_to_me.html'}"><i class="fi fi-rr-phone-call"></i> <span>Contact </span></a></li>
     <li><a href="${isIndex?'./pages/social_media.html': './social_media.html'}"><i class="fi fi-rr-share"></i> <span>Social Media</span></a></li>
 </ul>
 </nav>
